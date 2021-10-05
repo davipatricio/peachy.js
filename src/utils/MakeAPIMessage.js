3f29e07d2ba002c9d8ea1df8744e3368bc1a8db1
Davi <davipatricio2006@gmail.com>
2021-10-05 09:40:30 -0300

    feat: create new raw api message object utility

---
+++ src/utils/MakeAPIMessage.js
@@ -0,0 +1,23 @@
+const MessageEmbed = require('../structures/MessageEmbed');
+
+module.exports.transform = (data) => {
+	if (data.embeds) {
+		data.embeds?.map((embed) => {
+			if (embed instanceof MessageEmbed) {
+				return embed.toJSON();
+			}
+			return embed;
+		});
+	}
+
+
+	if (!data.content) {
+		data.content = '';
+	}
+
+	if (!data.tts) {
+		data.tts = false;
+	}
+
+	return data;
+};
