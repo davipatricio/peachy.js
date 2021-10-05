fe42bb741698af17b03337ddeefffb920e9f47e4
Davi <davipatricio2006@gmail.com>
2021-10-05 09:38:58 -0300

    feat: expose structures in main file

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 module.exports = {
 	Client: require('./src/client/Client'),
+	MessageEmbed: require('./src/structures/MessageEmbed'),
+	Guild: require('./src/structures/Guild'),
+	Message: require('./src/structures/Message'),
+	TextChannel: require('./src/structures/TextChannel'),
+	User: require('./src/structures/User'),
 };
