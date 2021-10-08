26706b6d44c876228908faebca38bdf5e6858b62
Davi <davipatricio2006@gmail.com>
2021-10-08 12:31:58 -0300

    refactor: remove unused code

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,5 @@
 'use strict';
 
-module.exports.isClass = (v, ...parameters) => {
-	const isConstructor = typeof v === 'function' && /^\s*class\s+/.test(v.toString());
-	if (isConstructor) {
-		// eslint-disable-next-line no-new
-		new (v, parameters);
-	}
-	return isConstructor;
-};
-
 module.exports = {
 	Cache: require('./Cache'),
 	CheckAPIError: require('./CheckAPIError'),
