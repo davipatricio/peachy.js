da39a52d46ed57e1f96cb79bce62d37a92b9dc1d
Davi <davipatricio2006@gmail.com>
2021-10-09 16:49:54 -0300

    fix: deepsource issues

--- src/actions/CHANNEL_UPDATE.js
+++ src/actions/CHANNEL_UPDATE.js
@@ -6,7 +6,7 @@ module.exports.handle = (client, data) => {
   switch (data.type) {
     case 0: {
       const oldChannel = client.channels.cache.get(data.id);
-      let channel;
+      let channel = null;
       if (oldChannel) {
         // We don't want to change the old channel data so we clone the old channel and update it's data
         channel = oldChannel._update(data);
--- src/actions/GUILD_UPDATE.js
+++ src/actions/GUILD_UPDATE.js
@@ -4,7 +4,7 @@ const Guild = require('../structures/Guild');
 
 module.exports.handle = (client, data) => {
   const oldGuild = client.guilds.cache.get(data.id);
-  let newGuild;
+  let newGuild = null;
   if (oldGuild) {
     // We don't want to change the old guild data so we clone the old guild and update it's data
     newGuild = oldGuild._update(data);
