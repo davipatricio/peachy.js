1fbeba2a51160cf7bd93ab94c9359ca8d366fbae
Davi <davipatricio2006@gmail.com>
2021-10-09 08:28:38 -0300

    feat: organize cases on CheckAPIError

--- src/utils/CheckAPIError.js
+++ src/utils/CheckAPIError.js
@@ -2,14 +2,14 @@
 
 module.exports.verifyForStatusCode = (endpoint, data, code) => {
   switch (code) {
-    case 403:
-      throw new Error(`DiscordAPIError: Missing Permissions (403)\nEndpoint: ${endpoint}\nData: ${data ?? 'empty'}`);
-    case 401:
-      throw new Error(`DiscordAPIError: Not Authorized (401)\nEndpoint: ${endpoint}\nData: ${data ?? 'empty'}`);
-    case 404:
-      throw new Error(`DiscordAPIError: Not Found (404)\nEndpoint: ${endpoint}\nData: ${data ?? 'empty'}`);
     case 400:
       throw new Error(`DiscordAPIError: Bad Request (400)\nEndpoint: ${endpoint}\nData: ${data ?? 'empty'}`);
+    case 401:
+      throw new Error(`DiscordAPIError: Not Authorized (401)\nEndpoint: ${endpoint}\nData: ${data ?? 'empty'}`);
+    case 403:
+      throw new Error(`DiscordAPIError: Missing Permissions (403)\nEndpoint: ${endpoint}\nData: ${data ?? 'empty'}`);
+    case 404:
+      throw new Error(`DiscordAPIError: Not Found (404)\nEndpoint: ${endpoint}\nData: ${data ?? 'empty'}`);
   }
 };
 
