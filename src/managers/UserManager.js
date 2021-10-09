43582f6924b1f007259636d403851001d6116cb0
Davi <davipatricio2006@gmail.com>
2021-10-09 10:03:06 -0300

    feat(UserManager): add fetch method

--- src/managers/UserManager.js
+++ src/managers/UserManager.js
@@ -1,11 +1,19 @@
 'use strict';
 
+const User = require('../structures/User');
 const LimitedMap = require('../utils/LimitedMap');
+const Requester = require('../utils/Requester');
 
 class UserManager {
   constructor(client, limit) {
     this.cache = new LimitedMap(limit);
     this.client = client;
+  }
+
+  async fetch(id) {
+    if (this.cache.has(id)) return this.cache.get(id);
+    const data = await Requester.create(this.client, `/users/${id}`, 'GET', true);
+    return new User(this.client, data);
   }
 }
 
