55e0730549194e0397bbd676d14dbc123fea4889
Davi <davipatricio2006@gmail.com>
2021-10-09 08:42:03 -0300

    fix: guilds not being added to cache

--- src/actions/GUILD_CREATE.js
+++ src/actions/GUILD_CREATE.js
@@ -4,6 +4,6 @@ const Guild = require('../structures/Guild');
 
 module.exports.handle = (client, data) => {
   const guild = new Guild(client, data);
-  client.guilds.cache.set(guild, guild.id);
+  client.guilds.cache.set(guild.id, guild);
   if (client.ready) client.emit('guildCreate', guild);
 };
--- src/utils/CacheFactory.js
+++ src/utils/CacheFactory.js
@@ -7,7 +7,7 @@ const UserManager = require('../managers/UserManager');
 
 module.exports.default = (options = {}) => ({
   EmojiManager: options.EmojiManager ?? Infinity,
-  GuildChannelManager: options.GuildChannelManager ?? Infinity,
+  ChannelManager: options.ChannelManager ?? Infinity,
   GuildManager: options.GuildManager ?? Infinity,
   RoleManager: options.RoleManager ?? Infinity,
   UserManager: options.UserManager ?? Infinity,
@@ -15,8 +15,8 @@ module.exports.default = (options = {}) => ({
 });
 
 module.exports.addToClient = (client, options) => {
-  client.guilds = new GuildManager(client, options.GuildChannelManager);
+  client.guilds = new GuildManager(client, options.GuildManager);
   client.emojis = new EmojiManager(client, options.EmojiManager);
   client.users = new UserManager(client, options.UserManager);
-  client.channels = new ChannelManager(client, options.GuildChannelManager);
+  client.channels = new ChannelManager(client, options.ChannelManager);
 };
