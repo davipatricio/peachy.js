9c97692c0711165d28f6f936c904b3f58176c71f
Davi <davipatricio2006@gmail.com>
2021-10-09 10:23:33 -0300

    feat(MessageDeleteAction): should set 'deleted' to true

--- src/actions/MESSAGE_DELETE.js
+++ src/actions/MESSAGE_DELETE.js
@@ -3,5 +3,6 @@
 module.exports.handle = (client, data) => {
   const channel = client.channels.cache.get(data.channel_id);
   const message = channel?.messages.cache.get(data.id);
+  if (message) message.deleted = true;
   client.emit('messageDelete', message ?? data);
 };
