ff76afd67e0e436bd3d1aa5b3dcdb665d439b976
Davi <davipatricio2006@gmail.com>
2021-10-09 13:40:17 -0300

    feat: support es modules

---
+++ index.mjs
@@ -0,0 +1,16 @@
+import Peachy from './index.js';
+
+export const {
+    Client,
+    MessageEmbed,
+    Guild,
+    Message,
+    TextChannel,
+    User,
+    EmojiManager,
+    GuildManager,
+    GuildChannelManager,
+    RoleManager,
+    UserManager,
+    version,
+  } = Peachy;
