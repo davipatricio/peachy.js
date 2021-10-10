00a2853e4bf7ab58635e6b414030cb02a123276b
Davi Patricio <davipatricio@pm.me>
2021-10-10 10:43:44 -0300

    feat(Invite): add channel & guild getters

--- src/structures/Invite.js
+++ src/structures/Invite.js
@@ -9,26 +9,28 @@ class Invite {
     this.parseData(data);
   }
 
+  get guild() {
+    return this.guildId ? this.client.guilds.cache.get(this.guildId) : null;
+  }
+
+  get channel() {
+    return this.channelId ? this.client.channels.cache.get(this.channelId) : null;
+  }
+
   parseData(data) {
     if (!data) return;
 
     this.code = data.code;
 
-    if (data.channel) {
-      this.channel = this.client.channels.cache.get(data.channel.id) ?? data.channel;
-      this.channelId = data.channel.id;
-    }
-
-    if (data.guild) {
-      this.guild = this.client.guilds.cache.get(data.guild.id) ?? data.guild;
-      this.guildId = data.guild.id;
-    }
+    this.channelId = data.channel ? data.channel.id : null;
+    this.guildId = data.guild ? data.guild.id : null;
 
     this.inviter = new User(this.client, data.inviter);
     this.maxAge = data.max_age;
     this.maxUses = data.max_uses;
     this.temporary = data.temporary;
     this.uses = data.uses;
+
     if (data.created_at) {
       this.createdTimestamp = new Date(data.created_at).getTime();
       this.createdAt = new Date(this.createdTimestamp);
