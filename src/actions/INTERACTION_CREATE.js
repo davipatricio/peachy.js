b5e0dd06ba258fc12ab8ef3dd0f4d8d0c9001d69
Davi Patricio <davipatricio@pm.me>
2021-10-11 11:01:33 -0300

    fix: deepsource issues

--- src/actions/INTERACTION_CREATE.js
+++ src/actions/INTERACTION_CREATE.js
@@ -4,7 +4,6 @@ const CommandInteraction = require('../structures/CommandInteraction');
 
 module.exports.handle = (client, data) => {
   let interaction = null;
-  console.log(data);
   switch (data.data.type) {
     // CHAT_INPUT (slash) command
     case 1: {
