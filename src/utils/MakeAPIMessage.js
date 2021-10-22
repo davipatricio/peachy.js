bfb5cd1ffce50073d082b433248e5cd18143b1f4
Davi Patricio <davipatricio@pm.me>
2021-10-22 17:38:15 -0300

    docs: document APIMessage class

--- src/utils/MakeAPIMessage.js
+++ src/utils/MakeAPIMessage.js
@@ -2,18 +2,26 @@
 
 const MessageEmbed = require('../structures/MessageEmbed');
 
-module.exports.transform = data => {
-  if (data.embeds) {
-    data.embeds?.map(embed => {
-      if (embed instanceof MessageEmbed) {
-        return embed.toJSON();
-      }
-      return embed;
-    });
-  }
+class APIMessage extends null {
+  
+  /**
+   * @param {Object} data - Custom message data to transform.
+   * @returns {Object} - Raw message data to send to Discord.
+   */
+  static transform(data) {
+    if (!data) throw new Error("Data should not be empty.")
+    if (data.embeds) {
+      data.embeds?.map(embed => {
+        if (embed instanceof MessageEmbed) {
+          return embed.toJSON();
+        }
+        return embed;
+      });
+    }
 
-  data.content ??= '';
-  data.tts ??= false;
+    data.content ??= '';
+    data.tts ??= false;
 
-  return data;
-};
+    return data;
+  };
+}
