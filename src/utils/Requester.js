f96745307c01481365be4da975caf3fc066b80a5
Davi Patricio <davipatricio@pm.me>
2021-10-22 17:45:03 -0300

    docs: document requester

--- src/utils/Requester.js
+++ src/utils/Requester.js
@@ -5,43 +5,49 @@ const { verifyForStatusCode, verifyForJSONStatusCode } = require('./CheckAPIErro
 const { apiUrl } = require('../constants/DiscordEndpoints');
 
 // "data" defaults to undefined because GET requests don't have a body
-module.exports.create = async (
-  client,
-  endpoint,
-  method = 'GET',
-  parseHeaders = true,
-  data = undefined,
-  headers = {},
-) => {
-  let parsedHeaders = headers;
-  if (parseHeaders) {
-    parsedHeaders = {
-      Authorization: `Bot ${client.token}`,
-      'Content-Type': 'application/json',
-      'User-Agent': `DiscordBot (https://github.com/DenkyLabs/peachy.js/, ${require('../../index').version})`,
-      ...headers,
-    };
+
+/** **/
+class Requester extends null {
+  /**
+   * @param {Client} client - The client making the request
+   * @param {string} endpoint - Which Discord endpoint make a request
+   * @param {string} [method=GET] - Which method to use in the request
+   * @param {boolean} [parseHeaders=true] - Should we include default headers (authorization, content-type, user-agent)
+   * @param {string|Object} [data] - JSON parameters to use in the request
+   * @param {Object} [headers={}] - Additional headers
+   */
+  static async create(client, endpoint, method = 'GET', parseHeaders = true, data = undefined, headers = {}) {
+
+    let parsedHeaders = headers;
+    if (parseHeaders) {
+      parsedHeaders = {
+        Authorization: `Bot ${client.token}`,
+        'Content-Type': 'application/json',
+        'User-Agent': `DiscordBot (https://github.com/DenkyLabs/peachy.js/, ${require('../../index').version})`,
+        ...headers,
+      };
+    }
+
+    const body = typeof data === 'object' ? JSON.stringify(data) : data;
+    const fetchData = await fetch(`${apiUrl(client.options.apiVersion)}${endpoint}`, {
+      method,
+      headers: parsedHeaders,
+      body,
+    });
+
+    let json = null;
+    try {
+      json = await fetchData.json();
+    } catch {
+      verifyForStatusCode(`${apiUrl(client.options.apiVersion)}${endpoint}`, data, fetchData.status);
+      return fetchData;
+    }
+
+    // Verify if an error code was returned from Discord API
+    // If there was an error, one of the following methods will throw an error
+    if (json.code) verifyForJSONStatusCode(json, `${apiUrl(client.options.apiVersion)}${endpoint}`, data, method);
+    verifyForStatusCode(`${apiUrl(client.options.apiVersion)}${endpoint}`, data, fetchData.status, method);
+
+    return json;
   }
-
-  const body = typeof data === 'object' ? JSON.stringify(data) : data;
-  const fetchData = await fetch(`${apiUrl(client.options.apiVersion)}${endpoint}`, {
-    method,
-    headers: parsedHeaders,
-    body,
-  });
-
-  let json = null;
-  try {
-    json = await fetchData.json();
-  } catch {
-    verifyForStatusCode(`${apiUrl(client.options.apiVersion)}${endpoint}`, data, fetchData.status);
-    return fetchData;
-  }
-
-  // Verify if an error code was returned from Discord API
-  // If there was an error, one of the following methods will throw an error
-  if (json.code) verifyForJSONStatusCode(json, `${apiUrl(client.options.apiVersion)}${endpoint}`, data, method);
-  verifyForStatusCode(`${apiUrl(client.options.apiVersion)}${endpoint}`, data, fetchData.status, method);
-
-  return json;
-};
+}
